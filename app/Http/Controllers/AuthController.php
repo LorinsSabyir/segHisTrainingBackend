670f9751cd1f89ce21a49db6602287a220f464af
Lei Lorenz Xavier <lorins.sabyir@gmail.com>
2026-07-27 17:02:02 +0800

    Implement user registration, login, and logout functionality with API routes and validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,30 +4,65 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
 
     public function register(Request $request)
     {
-        //
+        $fields = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $fields['password'] = Hash::make($fields['password']);
+
+        $user = User::create($fields);
+
+        $token = $user->createToken($request->name);
+
+        return [
+            'name' => $user->name,
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
     }
 
-    public function login(LoginRequest $request)
+    public function login(Request $request)
     {
-        return response()->json([
-            'message' => 'Validation passed.'
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required',
         ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if  ( !$user ||  !Hash::check($request->password, $user->password)) {
+            return [
+                'message' => 'Invalid Credentials.'
+            ];
+        }
+
+        $token = $user->createToken($user->name);
+
+        return [
+            'name' => $user->name,
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
     }
 
     public function logout(Request $request)
     {
-        //
+        $request->user()->tokens()->delete();
+
+        return [
+            'message' => 'You Logged Out.'
+        ];
     }
 
-    public function me(Request $request)
-    {
-        //
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,8 @@ use App\Http\Controllers\AuthController;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-
+// Auth Api
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/me', [AuthController::class, 'me']);
